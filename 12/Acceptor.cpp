d3d2b3e07f08234214a684492e7a32151d7d2311
MaxN320 <vfyzs9wl@gmail.com>
2024-06-19 00:49:56 +0800

    12

---
+++ 12/Acceptor.cpp
@@ -0,0 +1,45 @@
+#include "Acceptor.h"
+
+#include "Connection.h"
+
+// 处理新客户端连接请求。
+void Acceptor::newconnection()    
+{
+    InetAddress clientaddr;             // 客户端的地址和协议。
+    // 注意，clientsock只能new出来，不能在栈上，否则析构函数会关闭fd。
+    // 还有，这里new出来的对象没有释放，将在Connection类的析构函数中释放。
+    Socket *clientsock=new Socket(servsock_->accept(clientaddr));
+
+    printf ("accept client(fd=%d,ip=%s,port=%d) ok.\n",clientsock->fd(),clientaddr.ip(),clientaddr.port());
+
+    /*
+    // 为新客户端连接准备读事件，并添加到epoll中。
+    Channel *clientchannel=new Channel(loop_,clientsock->fd());   // 这里new出来的对象没有释放，这个问题以后再解决。
+    clientchannel->setreadcallback(std::bind(&Channel::onmessage,clientchannel));
+    clientchannel->useet();                 // 客户端连上来的fd采用边缘触发。
+    clientchannel->enablereading();   // 让epoll_wait()监视clientchannel的读事件
+    */
+    Connection *conn=new Connection(loop_,clientsock);   // 这里new出来的对象没有释放，这个问题以后再解决。
+}
+
+Acceptor::Acceptor(EventLoop* loop,const std::string &ip,const uint16_t port)
+{
+    servsock_=new Socket(createnonblocking());   
+    InetAddress servaddr(ip,port);             // 服务端的地址和协议。
+    servsock_->setreuseaddr(true);
+    servsock_->settcpnodelay(true);
+    servsock_->setreuseport(true);
+    servsock_->setkeepalive(true);
+    servsock_->bind(servaddr);
+    servsock_->listen();
+
+    acceptchannel_=new Channel(loop_,servsock_->fd());       
+    acceptchannel_->setreadcallback(std::bind(&Acceptor::newconnection,this));
+    acceptchannel_->enablereading();       // 让epoll_wait()监视servchannel的读事件。 
+}
+
+Acceptor::~Acceptor()
+{
+    delete servsock_;
+    delete acceptchannel_;
+}
---
+++ 12/Socket.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "InetAddress.h"
+
+//创建一个非阻塞的Socket
+int createnonblocking();
+
+class Socket
+{
+private:
+    const int fd_;    //Socket持有的fd，从构造函数传来
+public:
+    Socket(int fd);    //构造函数
+    ~Socket();
+
+    int fd()const ;  //返回fd_成员
+    void setreuseaddr(bool on); //设置SO_REUSEADDR选项，ture打开，false关闭
+    void setreuseport(bool on); //设置SO_REUSEPORT
+    void settcpnodelay(bool on); //设置SO_NODELAY
+    void setkeepalive(bool on);  //设置SO_KEEPALIVE
+
+    void bind(const InetAddress& servaddr); //服务端的socket将调用此函数
+    void listen(int nn=128);                
+    int accept(InetAddress& clientaddr);   
+};
