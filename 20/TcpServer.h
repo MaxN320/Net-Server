c5b68690d564170f1eac80cdd99dad1fcb1dec15
MaxN320 <vfyzs9wl@gmail.com>
2024-07-21 01:19:20 +0800

    20

---
+++ 20/TcpServer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include "EventLoop.h"
+#include "Socket.h"
+#include "Channel.h"
+#include "Acceptor.h"
+#include <map>
+
+#include "Connection.h"
+using namespace std;
+class TcpServer
+{
+private:
+    EventLoop loop_; //一个TcpSErver可以有多个事件循环
+    Acceptor *acceptor_; //一个TcpServer只有一个Accept类
+    std::map<int,Connection*> conns_;
+    // 一个TcpServer有多个Connection对象，存放在map容器中。
+public:
+    TcpServer(const std::string &ip,const uint16_t port);
+    ~TcpServer();
+
+    void start();
+
+    void newconnection(Socket *clientsock);    // 处理新客户端连接请求。
+    void closeconnection(Connection *conn);  // 关闭客户端的连接，在Connection类中回调此函数。 
+    void errorconnection(Connection *conn);  // 客户端的连接错误，在Connection类中回调此函数。
+    void onmessage(Connection*conn,std::string message);
+
+    void sendcomplete(Connection *conn); //发送完成后，Connection中回调此函数
+    void epolltimeout(EventLoop * loop); //epollwait超时，在Eventloop中回调此函数
+};
