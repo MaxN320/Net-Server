cdae5f1d5e1a9b0ab3e56ad651b6a46cdf5ae07a
MaxN320 <vfyzs9wl@gmail.com>
2024-08-03 16:16:37 +0800

    25

---
+++ 25/Buffer.cpp
@@ -0,0 +1,46 @@
+#include "Buffer.h"
+
+Buffer::Buffer()
+{
+
+}
+Buffer::~Buffer()
+{
+
+}
+
+void Buffer::append(const char *data,size_t size)
+{
+    buf_.append(data,size);
+}
+
+void Buffer::appendwithhead(const char*data,size_t size)
+{
+    buf_.append((char*)&size,4);
+    buf_.append(data,size);
+}
+
+// 返回buf_的大小。
+size_t Buffer::size()                                                            
+{
+    return buf_.size();
+}
+
+// 返回buf_的首地址。
+const char *Buffer::data()                                                  
+{
+    return buf_.data();
+}
+
+// 清空buf_。
+void Buffer::clear()                                                            
+{
+    buf_.clear();
+}
+
+
+// 从buf_的pos开始，删除nn个字节，pos从0开始。
+void Buffer::erase(size_t pos,size_t nn)                             
+{
+    buf_.erase(pos,nn);
+}
